cd9acc37a38a06396207c84ce764d702124bd439
Paulo Leonardo <admin@russelservicos.com.br>
2021-12-16 20:15:34 -0300

    'xxxxx'

--- nopadi/FS/Upload.php
+++ nopadi/FS/Upload.php
@@ -4,11 +4,14 @@
 *Author: Paulo Leonardo da Silva Cassimiro
 *
 */
-
 namespace Nopadi\FS;
 
   class Upload
     {
+	  private $code_error = null;
+	  private $messages = null;
+	  private $file_path = null;
+	  private $br = '<br>';
       private $uploadDir = 'uploads/';//pasta para onde os arquivos serão enviados
       private $allowedExtensions = array();//extensões permitidas para upload
       private $maxSize='2';//2mb  tamanho máximo do arquivo que pode ser enviado
@@ -57,6 +60,12 @@ namespace Nopadi\FS;
       {
         $this->randomName = $val;
       }
+	  
+	  public function getMessages()
+	  {
+		  return $this->messages;
+	  }
+	  
       /**
        * função que faz o upload do arquivo
        * a parâmetro $postFile é o arquivo enviado no formulário
@@ -66,17 +75,18 @@ namespace Nopadi\FS;
       {
         //verifica se existe algum erro ao enviar o arquivo. 
         //Se existir, exibe o erro e encerra a função
-        if($postFile['error'])
+        if(!is_null($postFile['error']))
         {
-          echo $this->getErrorMessage($postFile['error']);
-          return false;
-        }
+          $this->code_error = $postFile['error'];
+        }else{
+			$this->code_error = $postFile['error'];
+		}
         //verifica se o tamanho do arquivo é maior que o permitido. 
         //Se for maior exibe uma mensagem com essa informação e encerra a função
         if($this->getFileSize($postFile['size'])>$this->maxSize)
         {
-          echo 'O tamanho do arquivo enviado ('.number_format($this->getFileSize($postFile['size']),2).'mb) é maior do que o permitido(até '.$this->maxSize.'mb)';
-          return false;
+          $this->messages .= 'O tamanho do arquivo enviado ('.number_format($this->getFileSize($postFile['size']),2).'mb) é maior do que o permitido (até '.$this->maxSize.'mb)'.$this->br;
+		  exit;
         }
         //verifica se o tipo de arquivo enviado está na lista de arquivos permitidos
         //se for um tipo de arquivo não permitido, mostra uma mensagem informando que é um arquivo não permitido
@@ -84,70 +94,95 @@ namespace Nopadi\FS;
         $fileExt = $this->getFileExt($postFile['name']);//obtém o tipo de arquivo
         if(!in_array($fileExt, $this->allowedExtensions))
         {
-          echo 'Tipo de arquivo inválido => '.$fileExt;
-          echo '<br>São aceitos apenas aquivos do tipo => '. implode(', ', $this->allowedExtensions);
-          return false;
+          $this->messages .= 'Tipo de arquivo inválido ('.$fileExt.').'.$this->br;
+          $this->messages .= 'São aceitos apenas aquivos do tipo: '. implode(', ', $this->allowedExtensions).'.'.$this->br;
+		  return false;
+		  exit;
         }
         //verifica se a pasta que o arquivo será enviado existe.
         //Se não existir encerra a função e informa que a pasta não existe
           if(!$this->hasUploadDir($this->uploadDir))
           {
-            echo 'Diretório de upload não existe';
+            $this->messages .= 'Diretório de upload não existe'.$this->br;
             return false;
+			exit;
           }
           //a linha abaixo atribui um nome ao arquivo
           //se for definido para gerar um nome aleatório ele gera um nome aleatório, senão, ele mantém o nome original do arquivo
           $fileName = $this->randomName ? date('dmyhis').rand().'.'.$fileExt : $postFile['name'];
-          
+
           //tenta mover o arquivo enviado através do formulário html para a pasta de upload informada
-          if(move_uploaded_file($postFile['tmp_name'], $this->uploadDir.'/'.$fileName))
+		  $file_path = $this->uploadDir.'/'.$fileName;
+          if(move_uploaded_file($postFile['tmp_name'], $file_path))
           {
+			$this->file_path = $file_path;
+			$this->messages = "Arquivo:\"{$fileName}\" enviado com sucesso para o servidor.";
             return $fileName;
           }
           else
           {
-           return false;
+            return false;
           }
-        
       }
+	  
+	  /*Retorna a pasta ao qual o arquivo foi salvo*/
+	  public function getFilePath()
+	  {
+		  return $this->file_path;
+	  }
       
       //obtém a mensagem de erro do upload através do código de erro informado
-       private function getErrorMessage($code) 
-        { 
+       public function getErrorMessage($code=null) 
+       { 
+		    $code  = is_null($code) ? $this->code_error : $code;
             switch ($code) { 
                 case UPLOAD_ERR_INI_SIZE: 
-                  $message = 'O arquivo enviado excede o tamanho máximo de envio da diretiva upload_max_filesize do php.ini';
+                  $message = 'O arquivo enviado excede o tamanho máximo de envio da diretiva upload_max_filesize do php.ini.';
                     break; 
                 case UPLOAD_ERR_FORM_SIZE: 
-                  $message = 'O arquivo enviado excede a directive MAX_FILE_SIZE que foi especificada o formulário HTML';
+                  $message = 'O arquivo enviado excede a directive MAX_FILE_SIZE que foi especificada o formulário HTML.';
                     break; 
                 case UPLOAD_ERR_PARTIAL: 
                   $message = 'O arquivo enviado foi enviado parcialmente';
                     break; 
                 case UPLOAD_ERR_NO_FILE: 
-                    $message = 'Nenhum arquivo foi enviado'; 
+                    $message = 'Nenhum arquivo foi enviado.'; 
                     break; 
                 case UPLOAD_ERR_NO_TMP_DIR: 
-                    $message = 'Não é possível encontrar a pasta temporária'; 
+                    $message = 'Não é possível encontrar a pasta temporária.'; 
                     break; 
                 case UPLOAD_ERR_CANT_WRITE: 
-                    $message = 'Falha ao escrever o arquivo no disco'; 
+                    $message = 'Falha ao escrever o arquivo no disco.'; 
                     break; 
                 case UPLOAD_ERR_EXTENSION: 
-                  $message = 'Uma extensão do PHP interompeu o envio do arquivo';
+                  $message = 'Uma extensão do PHP interompeu o envio do arquivo.';
                     break; 
+				case UPLOAD_ERR_OK: 
+                   $message = 'Arquivo enviado com sucesso para o servidor.';
+                    break;
                 default: 
-                    $message = 'Erro de envio desconhecido'; 
+                    $message = 'Erro de envio desconhecido.'; 
                     break; 
             } 
-            return $message; 
+			
+			$x = $message.$this->br.$this->messages;
+			if($this->messages){
+				$x = str_ireplace($message,'',$x);
+			    $x = substr($x,strlen($this->br));
+			}
+			
+            return $x; 
         }
         //obtém o tipo de arquivo
         private function getFileExt($fileName)
         {
+		  if(strlen($fileName) > 2){
           preg_match("/\.[a-zA-Z]{2,4}$/", $fileName,$matches);
           $fileExt = strtolower(str_replace('.', '',$matches[0]));
           return $fileExt;
+		  }else{
+			  $this->messages .= 'Não foi possível obter o caminho do arquivo.'.$this->br;
+		  }
         }
         /** 
          * verifica se a pasta de upload existe
